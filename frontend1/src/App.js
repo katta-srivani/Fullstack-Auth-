6ca22b280fce08d3fa92010a237520de0b741633
Srivani Katta <kattasrivani74@gmail.com>
2026-03-20 14:17:00 +0530

    fixed unused AuthProvider import

--- frontend1/src/App.js
+++ frontend1/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AuthProvider from "./context/AuthContext";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";   // ✅ ADD THIS
 import ForgotPassword from "./pages/ForgotPassword";
